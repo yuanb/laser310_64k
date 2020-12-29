1cd15f3d0f365e9723d841c714c0e7aeb0905687
yuanb <yuanb@users.noreply.github.com>
2020-12-28 23:15:31 -0500

    Added paging support and tests

--- laser310_64k_tb.v
+++ laser310_64k_tb.v
@@ -25,13 +25,14 @@
 module ram64k_tb;
 
 	// Inputs
-	reg clk;
 	reg [4:0] Addr;
 	reg [3:0] AddrIO;	//A7 - A4,
 	reg WR_N;
 	reg RD_N;
 	reg MREQ_N;
 	reg IORQ_N;
+	reg clk;
+	reg RESET_N;
 	reg [1:0] D1D0;
 
 	// Outputs
@@ -39,22 +40,26 @@ module ram64k_tb;
 	wire RAM_CS_N;
 	wire RAM_OE_N;
 	wire RAM_WE_N;
+	wire [1:0] bank;
 	wire led1;
 	wire led2;
 	
-	reg[95:0] testcase = "Test#1......";
+	reg[95:0] testcase = "RESET_N     ";
 	reg[39:0] error_msg = "Good!";
+	
+	parameter CLOCK_PERIOD = 20;
 	 
 	// Instantiate the Unit Under Test (UUT)
 	top uut (
 		/*Input*/
-		.clk(clk),
 		.Addr(Addr), 
 		.AddrIO(AddrIO),
 		.WR_N(WR_N),
 		.RD_N(RD_N),
 		.MREQ_N(MREQ_N),
 		.IORQ_N(IORQ_N),
+		.clk(clk),
+		.RESET_N(RESET_N),
 		.D1D0(D1D0),
 		
 		/*Output*/
@@ -62,17 +67,32 @@ module ram64k_tb;
 		.RAM_CS_N(RAM_CS_N), 
 		.RAM_OE_N(RAM_OE_N),
 		.RAM_WE_N(RAM_WE_N),
+		.bank(bank),
 		.led1(led1),
 		.led2(led2)
 	);
 
    always  
-		#10  clk = !clk; 
+		#(CLOCK_PERIOD/2)  clk = !clk; 
  
 	initial begin
+		//Test0. RESET	
+		RESET_N = 0;
 		clk = 0;
+		#50;
+		RESET_N = 1;
+		
+		if (RAM_CS_N !== 1'bx || bank !== 2'b01)
+			begin
+				$display("Test0, failed, RAM_CS_N is not 1 || bank is not 2'b01");
+				error_msg = "Error";
+			end
+		else
+			error_msg = "Good!";		
+
 		//Test1. Invalid RD/WR
-		Addr = 5'bxxxxx;
+		#50;
+		Addr = 5'b10111;
 		AddrIO = 4'bxxxx;
 		WR_N = 1;
 		RD_N = 1;
@@ -83,33 +103,37 @@ module ram64k_tb;
 		#1;
 		if (RAM_CS_N !== 1)
 			begin
-				$display("Test1, RAM_CS_N is not 1 (RD=WR=1)");
+				$display("Test1, failed, RAM_CS_N is not 1 (RD=WR=1)");
 				error_msg = "Error";
 			end
 		else
-			#50;
-			WR_N = 0;
-			RD_N = 0;
-			if (RAM_CS_N !== 1)
-				begin
-					$display("Test1, RAM_CS_N is not 1(RD=WR=0)");
-					error_msg = "Error";				
-				end
-			else
-				#50
-				WR_N = 1;
-				RD_N = 1;
+			begin
+				#50;
+				WR_N = 0;
+				RD_N = 0;
 				if (RAM_CS_N !== 1)
 					begin
-						$display("Test1, RAM_CS_N is not 1(RD=WR=0 to RD=WR=1)");
-						error_msg = "Error";						
+						$display("Test1, RAM_CS_N is not 1(RD=WR=0)");
+						error_msg = "Error";				
 					end
 				else
-					error_msg = "Good!";
+					begin
+						#50
+						WR_N = 1;
+						RD_N = 1;
+						if (RAM_CS_N !== 1)
+							begin
+								$display("Test1, RAM_CS_N is not 1(RD=WR=0 to RD=WR=1)");
+								error_msg = "Error";						
+							end
+						else
+							error_msg = "Good!";
+					end
+			end
 
 		//Test2. Invalid MREQ/IORQ
 		#50;
-		Addr = 5'bxxxxx;
+		Addr = 5'b10111;
 		AddrIO = 4'bxxxx;
 		WR_N = 0;
 		RD_N = 1;
@@ -123,25 +147,29 @@ module ram64k_tb;
 				error_msg = "Error";
 			end
 		else
-			#50;
-			MREQ_N = 0;
-			IORQ_N = 0;
-			if (RAM_CS_N !== 1)
-				begin
-					$display("Test2, RAM_CS_N is not 1(MREQ=IREQ=0)");
-					error_msg = "Error";				
-				end
-			else
+			begin
 				#50;
-				MREQ_N = 1;
-				IORQ_N = 1;
+				MREQ_N = 0;
+				IORQ_N = 0;
 				if (RAM_CS_N !== 1)
 					begin
-					$display("Test2, RAM_CS_N is not 1(MREQ=IREQ=0 to MREQ=IREQ=1)");
-					error_msg = "Error";						
+						$display("Test2, RAM_CS_N is not 1(MREQ=IREQ=0)");
+						error_msg = "Error";				
 					end
 				else
-					error_msg = "Good!";
+					begin
+						#50;
+						MREQ_N = 1;
+						IORQ_N = 1;
+						if (RAM_CS_N !== 1)
+							begin
+								$display("Test2, RAM_CS_N is not 1(MREQ=IREQ=0 to MREQ=IREQ=1)");
+								error_msg = "Error";						
+							end
+						else
+							error_msg = "Good!";
+					end
+			end
 		
 		//Test3. B8xxH write access
 		#50;
@@ -213,7 +241,7 @@ module ram64k_tb;
 		#1;
 		if (RAM_CS_N !==0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b01)
 			begin
-				$display("Test6, failed, expceting !CS, WE, !OE, A15A14= 01.");
+				$display("Test6, failed, expceting !CS, WE, !OE, A15A14= 2'b01.");
 				error_msg = "Error";
 			end
 		else
@@ -231,7 +259,7 @@ module ram64k_tb;
 		testcase = "7. FFFFH /RD";
 		if (RAM_CS_N !==0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b01)
 			begin
-				$display("Test7, failed, expceting !CS, WE, !OE, A15A14= 01.");
+				$display("Test7, failed, expceting !CS, WE, !OE, A15A14= 2'b01.");
 				error_msg = "Error";
 			end
 		else
@@ -249,22 +277,49 @@ module ram64k_tb;
 		testcase = "8. FFFFH /RD";
 		if (RAM_CS_N !==0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b01)
 			begin
-				$display("Test8, failed, expceting !CS, WE, !OE, A15A14= 01.");
+				$display("Test8, failed, expceting !CS, WE, !OE, A15A14= 2'b01.");
 				error_msg = "Error";
 			end
 		else
-			#49;
-			Addr = 5'b10111;
-			#1;
-			if (RAM_CS_N !== 0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b00)
-				begin
-					$display("Test8, invalid output");
-					error_msg = "Error";
-				end
-			else
-				error_msg = "Good!";
+			begin
+				#49;
+				Addr = 5'b10111;
+				#1;
+				if (RAM_CS_N !== 0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b00)
+					begin
+						$display("Test8, invalid output");
+						error_msg = "Error";
+					end
+				else
+					error_msg = "Good!";
+			end
 				
-		//Test9. Switch to bank 0 and 1
+		//Test9. C000H Switch to bank 2
+		#49;
+		Addr = 5'b11000;
+		AddrIO = 4'b0111;
+		#(CLOCK_PERIOD/2);
+		WR_N = 0;
+		RD_N = 1;
+		MREQ_N = 1;
+		IORQ_N = 0;
+		D1D0 = 2'b10;
+		#(CLOCK_PERIOD/2);
+		WR_N = 1;
+		RD_N = 0;
+		MREQ_N = 0;
+		IORQ_N = 1;
+		D1D0 = 2'bxxx;
+		testcase = "9. BANK2 /RD";
+		#1;
+		if (RAM_CS_N !==0 || RAM_WE_N !== 1 || RAM_OE_N !== 0 || RAM_A1514 !== 2'b10)
+			begin
+				$display("Test9, failed, expceting !CS, WE, !OE, A15A14= 2'b10.");
+				error_msg = "Error";
+			end
+		else
+			error_msg = "Good!";
+		
 		
 		//Test10. Switch to bank 2
 		
--- top.v
+++ top.v
@@ -4,15 +4,16 @@
 //Addr = A15 - A11
 //A15 = Addr<4>, A14 = Addr<3>, A13 = Addr<2>, A12 = Addr<1>, A11 = Addr<0>
 //AddrIO = A7,A6,A5,A4
-module top(/*Input*/ clk, Addr, AddrIO, WR_N, RD_N, MREQ_N, IORQ_N, D1D0,/*Output*/ RAM_A1514, RAM_CS_N, RAM_OE_N, RAM_WE_N, /*Led*/ led1, led2);
+module top(/*Input*/Addr, AddrIO, WR_N, RD_N, MREQ_N, IORQ_N, clk, RESET_N, D1D0, /*Output*/ RAM_A1514, RAM_CS_N, RAM_OE_N, RAM_WE_N, bank, /*Led*/ led1, led2);
 
-	input wire clk;
 	input wire[4:0] Addr;
 	input wire[3:0] AddrIO;
 	input wire WR_N;
 	input wire RD_N;
 	input wire MREQ_N;
 	input wire IORQ_N;
+	input wire clk;
+	input wire RESET_N;
 	input wire [1:0] D1D0;
 	
 	output wire[1:0] RAM_A1514;	
@@ -23,6 +24,8 @@ module top(/*Input*/ clk, Addr, AddrIO, WR_N, RD_N, MREQ_N, IORQ_N, D1D0,/*Outpu
 	output wire led1;
 	output wire led2;
 	
+	output reg[1:0] bank;
+	
 	//RAM decoding
 	//Laser 310 - Expension RAM B800h
 	//B800h = 1011 1000 0000 0000
@@ -31,16 +34,18 @@ module top(/*Input*/ clk, Addr, AddrIO, WR_N, RD_N, MREQ_N, IORQ_N, D1D0,/*Outpu
 	//FFFFh = 1111 1111 1111 1111
 	//Mask = 1111 1000 0000 0000
 	//A15,A14,A13,A12,A11 range: [10111,11111] = [17,1F]
-
-	reg[1:0] bank;
-
-	initial bank = 2'b01;
-	
-	always @(posedge clk)
+	always @(posedge clk or negedge RESET_N)
 	begin
-		if (IORQ_N == 1'b0 && MREQ_N == 1'b1 && WR_N==1'b0 && RD_N==1'b1 && AddrIO == 4'b0111)
-			bank = D1D0;
+		if (~RESET_N)
+			bank = 2'b01;
+		else
+			begin
+				if (IORQ_N == 1'b0 && MREQ_N == 1'b1 && WR_N==1'b0 && RD_N==1'b1 && AddrIO == 4'b0111)
+					bank = D1D0;
+			end
 	end
+	
+	//TODO, move the following to always block
 	
 	// Laser 310 64K bank 0,1 -> RAM page 0, page 1 -> RAM 17:0000h-07FFh, 18-1f: 4000h-7FFFh 
 	// Laser 310 64K bank 0,2 -> RAM page 0, page 2 -> RAM 17:0000h-07FFh, 18-1f: 8000h-7FFFh
@@ -48,16 +53,17 @@ module top(/*Input*/ clk, Addr, AddrIO, WR_N, RD_N, MREQ_N, IORQ_N, D1D0,/*Outpu
 	//4000:0100 0000 0000 0000 + 0,7 on high bits -> 01000, 01111
 	//8000:1000 0000 0000 0000 + 0,7 on high bits -> 10000, 10111
 	//C000:1100 0000 0000 0000 = 0,7 on high bits -> 11000, 11111
-	assign RAM_A1514 = (Addr == 5'b1011_1) ? 2'b00 : 
+	assign RAM_A1514 =(Addr == 5'b1011_1) ? 2'b00 : 
 							(bank == 2'b01) ? 2'b01 : 
 							(bank == 2'b10) ? 2'b10 :
 							(bank == 2'b11) ? 2'b11 : 2'b01;
 
 	//RAM_CS_N is low if : 1. /MREQ; 2. Valid Addr;	3. WR and RD are valid; 4. MREQ and IORQ are valid.
 	assign RAM_CS_N = (MREQ_N==1'b0 && 
-							/*validLaser310Space*/ (Addr>=5'b1011_1 && Addr<=5'b1111_1) &&
-							/*validWRRD*/ ((WR_N==0 & RD_N==1) | (WR_N==1 & RD_N==0)) && 
-							/*validMREQIORQ*/ ((MREQ_N==0 & IORQ_N==1) | (MREQ_N==1 & IORQ_N==0))) ? 1'b0 : 1'b1;
+							(Addr>=5'b1011_1 && Addr<=5'b1111_1) && /*validLaser310Space*/
+							((WR_N==0 & RD_N==1) | (WR_N==1 & RD_N==0)) && /*validWRRD*/
+							((MREQ_N==0 & IORQ_N==1) | (MREQ_N==1 & IORQ_N==0))) /*validMREQIORQ*/
+							? 1'b0 : 1'b1;
 	
 	assign RAM_OE_N = (RAM_CS_N==0 && WR_N==1) ? 1'b0 : 1'b1;
 	assign RAM_WE_N = (RAM_CS_N==0 && WR_N==0) ? 1'b0 : 1'b1;
